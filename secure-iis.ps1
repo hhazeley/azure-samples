206f34148317364a3cad7463b44a011ed830ee97
Iain Foulds <iain.foulds@microsoft.com>
2017-07-12 10:22:59 -0700

    Update secure-iis.ps1

--- secure-iis.ps1
+++ secure-iis.ps1
@@ -1,2 +1,2 @@
-Import-Module WebAdministration | Get-ChildItem cert:\localmachine\My | New-Item -Path IIS:\SslBindings\!443
+Get-ChildItem cert:\localmachine\My | New-Item -Path IIS:\SslBindings\!443
 New-WebBinding -Name "Default Web Site" -Protocol https -Port 443
