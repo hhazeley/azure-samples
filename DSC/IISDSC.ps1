185b304ced2d3bf40a7c35a984026966f4dc1748
Hannel <hahaze@microsoft.com>
2020-03-09 15:56:17 -0700

    Update TestScript to catch rerun correctly.

--- DSC/IISDSC.ps1
+++ DSC/IISDSC.ps1
@@ -4,10 +4,6 @@ configuration InstallConfigureIIS
 
     Node localhost
     {
-        $title = "<title>$env:COMPUTERNAME - ITOpsTalk</title>"
-        $page = gc "C:\inetpub\wwwroot\iisstart.htm"
-        $imgurl = "https://i.imgur.com/N3ZK6b4.png"
-
         LocalConfigurationManager
             {
                 ConfigurationMode    = "ApplyAndAutoCorrect"
@@ -23,16 +19,18 @@ configuration InstallConfigureIIS
             {
         SetScript = {
             $webpage = gc "C:\inetpub\wwwroot\iisstart.htm"
-            $webpage = $webpage -replace "<title>.*</title>","$using:title"
-            $webpage = $webpage -replace "iisstart.png","$using:imgurl"
+            $webpage = $webpage -replace "<title>.*</title>","<title>$env:COMPUTERNAME - ITOpsTalk</title>"
+            $webpage = $webpage -replace "iisstart.png","https://i.imgur.com/N3ZK6b4.png"
             $webpage | Out-File -FilePath C:\inetpub\wwwroot\iisstart.htm -Force
             }
         TestScript = {
-            $test = $using:page -contains $using:title
-            return $test
+            $title = "<title>$env:COMPUTERNAME - ITOpsTalk</title>"
+            $page = gc "C:\inetpub\wwwroot\iisstart.htm"
+            $test = $page -contains $title
+            $test
             }
         GetScript = {
-            @{'Result' = $using:page -contains $using:title}
+            @{'Result' = (Test-Path "C:\inetpub\wwwroot\iisstart.htm")}
             }
         DependsOn = "[WindowsFeature]InstallIIS"
         }
